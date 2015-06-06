4607022e5a0bf39cc25922e3d6fbf709922dde3e
Fredrik Jonsén <fredrikjonsen@gmail.com>
2015-06-06 20:01:25 +0200

    Finished lab1

--- lab1/chat.js
+++ lab1/chat.js
@@ -1,6 +1,9 @@
+
+
+
 $(document).ready(function() {
   $("#sendButton").click(function() {
-    var tAC = $("#msgInput").val();
+    var tAC = $("#msgInput").val().trim();
     if (tAC.length > 0 && tAC.length <= 140) {
       $("#messages").prepend(constructMessage(tAC));
       $("#warning").toggle(false);
@@ -15,8 +18,17 @@ var constructMessage = function(text)
 {
   var container = $("<div/>", {class: "messageContainer unread"});
   var alignDiv = $("<div/>", {class: "alignDiv"});
-  $("<input/>", {type: "checkbox"}).appendTo(alignDiv);
   $("<p/>", {class: "messageText"}).text(text).appendTo(alignDiv);
+
+  var readFunc = function() {
+    $(this).parent().removeClass("unread").addClass("read");
+    $(this).prop("disabled", true);
+    // Outline: none for some reason not working on focus, using this
+    // as workaround
+    $(this).blur();
+  };
+
+  $("<input/>", {type: "checkbox"}).click(readFunc).appendTo(alignDiv);
   container.append(alignDiv);
   return container;
 }
