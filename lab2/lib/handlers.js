45bac7c83865e254374220dc7a485393e6559c22
Fredrik Jonsén <fredrikjonsen@gmail.com>
2015-08-08 08:23:40 +0200

    Added code coverage result

--- lab2/lib/handlers.js
+++ lab2/lib/handlers.js
@@ -8,46 +8,50 @@ var Handler = function(collection){
   var collection = collection;
 
   this.save = function(msg, done) {
+    done = done || function() {};
     msg = msg.trim();
     if (typeof msg !== "string" || msg.length == 0 || msg.length > 140)
     {
-      done && done(new ArgumentError("Invalid message"), false);
+      done(new ArgumentError("Invalid message"), false);
       return;
     }
     collection.insertOne({'message': msg, 'flag': false}, function(err, r) {
       if (err) done && done(new DatabaseError(err), false);
-      else done && done(null, r.ops[0]);
+      else done(null, r.ops[0]);
     });
 
   };
 
   this.flag = function(msgId, done) {
+    done = done || function() {};
     if (!ObjectID.isValid(msgId)) {
-      done && done(new ArgumentError("Invalid id"), false);
+      done(new ArgumentError("Invalid id"), false);
       return;
     }
     collection.updateOne({_id: new ObjectID(msgId)}, {'flag': true}
       , function(err, r) {
         if (err) {
-          done && done(new DatabaseError(err), false);
+          done(new DatabaseError(err), false);
         }
         else if (r.result['nModified'] == 0) {
-           done && done(new ArgumentError("No message with id" + msgId), false);
+           done(new ArgumentError("No message with id" + msgId), false);
         }
         else {
-          done && done(null, true);
+          done(null, true);
         }
     });
   };
 
   this.getall = function(done) {
-      collection.find().toArray(function(err, result) {
-        result.map(function(doc) {
-          doc['_id'] = doc['_id'].toString();
-        });
-        if(err) done && done(new DatabaseError(err), null)
-        else done && done(null, result);
+    done = done || function() {};
+
+    collection.find().toArray(function(err, result) {
+      result.map(function(doc) {
+        doc['_id'] = doc['_id'].toString();
       });
+      if(err) done(new DatabaseError(err), null)
+      else done(null, result);
+    });
   };
 };
 
--- lab2/test/test.js
+++ lab2/test/test.js
@@ -1,8 +1,9 @@
 var should = require('should');
 var assert = require('assert');
 
-var Handler = require('../lib-coverage/handlers.js');
-var errors = require('../lib-coverage/errors.js');
+require('blanket');
+var Handler = require('../lib/handlers.js');
+var errors = require('../lib/errors.js');
 var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 
@@ -11,6 +12,16 @@ describe('Handlers', function() {
   var collectionName = 'test_chat';
   var collection = null;
   var handler = null;
+
+  var cleanDb = function(done) {
+    // Clear collection after each test
+
+    collection.remove({});
+    collection.count(function(err, count) {
+      count.should.equal(0);
+      done();
+    });
+  };
 
   before('setup', function(done) {
     MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, _db) {
@@ -23,166 +34,172 @@ describe('Handlers', function() {
     });
   });
 
-  afterEach(function(done) {
-    // Clear collection after each test
-    collection.remove({});
-    collection.count(function(err, count) {
-      count.should.equal(0);
-      done();
-    });
-  });
+  describe('Save', function() {
+    afterEach(cleanDb);
 
-  describe('Save empty message', function() {
-    it('should return error', function(done) {
-      handler.save('', function(err, result) {
+    describe('Save empty message', function() {
+      it('should return error', function(done) {
+        handler.save('', function(err, result) {
 
-        // Should return error and database should be empty
-        err.should.be.instanceof(errors.ArgumentError);
-        collection.count(function(err, count) {
-          count.should.equal(0);
-          done();
-        })
-      });
-    });
-  });
-
-  describe('Save short, valid, message', function() {
-    it('should return true', function(done) {
-      handler.save('message', function(err, result) {
-        result.should.be.ok();
-        collection.count(function(err, count) {
-          count.should.equal(1);
-          done();
-        })
-      });
-    });
-  });
-
-  describe('Save too long message', function() {
-    it('should return error', function(done) {
-      handler.save(Array(142).join("a"), function(err, result) {
-        err.should.be.instanceof(errors.ArgumentError);
-        collection.count(function(err, count) {
-          count.should.equal(0);
-          done();
-        })
-      });
-    });
-  });
-
-  describe('Save whitespace-only message', function() {
-    it('should return error', function(done) {
-      handler.save(' ', function(err, result) {
-        err.should.be.instanceof(errors.ArgumentError);
-        collection.count(function(err, count) {
-          count.should.equal(0);
-          done();
-        });
-      });
-    });
-  });
-
-  describe('Save 140-character message with trailing whitespace', function() {
-    it('should be trimmed and pass', function(done) {
-      var msg = Array(141).join("a") + " ";
-      handler.save(msg, function(err, result) {
-        result.should.be.ok();
-        collection.count(function(err, count) {
-          count.should.equal(1);
-          done();
-        });
-      });
-    });
-  });
-
-  describe('Flag existing message', function() {
-    var msgId = null;
-    before('save a message to flag', function(done) {
-      handler.save("a", function(err, result) {
-        msgId = result["_id"].toString();
-        collection.count(function(err, count) {
-          count.should.equal(1);
-          done();
-        })
-      });
-    });
-
-    it('should return true', function(done) {
-      handler.flag(msgId, function(err, r) {
-        r.should.be.ok();
-        collection.find().toArray(function(err, docs) {
-          docs[0]['flag'].should.be.ok();
-          done();
-        });
-      });
-    });
-  });
-
-  describe('Flag invalid id', function() {
-    it('should return an error', function(done) {
-      handler.flag('a', function(err, r) {
-        err.should.be.instanceof(errors.ArgumentError);
-        collection.count(function(err, count) {
-          count.should.equal(0);
-          done();
-        })
-      })
-    });
-  });
-
-  describe('Flag non-existant message', function() {
-    it('should return an error', function(done) {
-      handler.flag('123456789012123456789012', function(err, r) {
-        err.should.be.instanceof(errors.ArgumentError);
-        collection.count(function(err, count) {
-          count.should.equal(0);
-          done();
-        });
-      });
-    });
-  });
-
-  describe('Get message', function() {
-    before('save a message to later get', function(done) {
-      handler.save('a message', function(err, r) {
-        collection.count(function(err, count) {
-          count.should.equal(1);
-          done();
+          // Should return error and database should be empty
+          err.should.be.instanceof(errors.ArgumentError);
+          collection.count(function(err, count) {
+            count.should.equal(0);
+            done();
+          })
         });
       });
     });
 
-    it('should return exactly one message', function(done) {
-      handler.getall(function(err, result) {
-        result.length.should.equal(1);
-        result[0]['message'].should.equal('a message');
-        done();
+    describe('Save short, valid, message', function() {
+      it('should return true', function(done) {
+        handler.save('message', function(err, result) {
+          result.should.be.ok();
+          collection.count(function(err, count) {
+            count.should.equal(1);
+            done();
+          })
+        });
       });
     });
-  });
 
-  describe('Get several messages', function() {
-    before('save three messages to later get', function(done) {
-      handler.save('message 1', function(err, r) {
-        handler.save('message 2', function(err, r) {
-          handler.save('message 3', function(err, r) {
-            collection.count(function(err, count) {
-              count.should.equal(3);
-              done();
-            });
+    describe('Save too long message', function() {
+      it('should return error', function(done) {
+        handler.save(Array(142).join("a"), function(err, result) {
+          err.should.be.instanceof(errors.ArgumentError);
+          collection.count(function(err, count) {
+            count.should.equal(0);
+            done();
+          })
+        });
+      });
+    });
+
+    describe('Save whitespace-only message', function() {
+      it('should return error', function(done) {
+        handler.save(' ', function(err, result) {
+          err.should.be.instanceof(errors.ArgumentError);
+          collection.count(function(err, count) {
+            count.should.equal(0);
+            done();
           });
         });
       });
     });
 
-    it('should return exactly three messages', function(done) {
-      handler.getall(function(err, result) {
-        result.length.should.equal(3);
-        result = result.map(function(i) { return i['message']; });
-        result.should.eql(['message 1', 'message 2', 'message 3']);
-        done();
+    describe('Save 140-character message with trailing whitespace', function() {
+      it('should be trimmed and pass', function(done) {
+        var msg = Array(141).join("a") + " ";
+        handler.save(msg, function(err, result) {
+          result.should.be.ok();
+          collection.count(function(err, count) {
+            count.should.equal(1);
+            done();
+          });
+        });
       });
     });
+  });
+
+  describe('Flag', function() {
+
+    afterEach(cleanDb);
+
+    describe('Flag existing message', function() {
+      var msgId = null;
+      before('save a message to flag', function(done) {
+        handler.save("a", function(err, result) {
+          msgId = result["_id"].toString();
+          collection.count(function(err, count) {
+            count.should.equal(1);
+            done();
+          })
+        });
+      });
+
+      it('should return true', function(done) {
+        handler.flag(msgId, function(err, r) {
+          r.should.be.ok();
+          collection.find().toArray(function(err, docs) {
+            docs[0]['flag'].should.be.ok();
+            done();
+          });
+        });
+      });
+    });
+
+    describe('Flag invalid id', function() {
+      it('should return an error', function(done) {
+        handler.flag('a', function(err, r) {
+          err.should.be.instanceof(errors.ArgumentError);
+          collection.count(function(err, count) {
+            count.should.equal(0);
+            done();
+          })
+        })
+      });
+    });
+
+    describe('Flag non-existant message', function() {
+      it('should return an error', function(done) {
+        handler.flag('123456789012123456789012', function(err, r) {
+          err.should.be.instanceof(errors.ArgumentError);
+          collection.count(function(err, count) {
+            count.should.equal(0);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  describe('Getall', function() {
+
+    afterEach(cleanDb);
+
+    describe('Get message', function() {
+      before('save a message to later get', function(done) {
+        handler.save('a message', function(err, r) {
+          collection.count(function(err, count) {
+            count.should.equal(1);
+            done();
+          });
+        });
+      });
+
+      it('should return exactly one message', function(done) {
+        handler.getall(function(err, result) {
+          result.length.should.equal(1);
+          result[0]['message'].should.equal('a message');
+          done();
+        });
+      });
+    });
+
+    describe('Get several messages', function() {
+      before('save three messages to later get', function(done) {
+        handler.save('message 1', function(err, r) {
+          handler.save('message 2', function(err, r) {
+            handler.save('message 3', function(err, r) {
+              collection.count(function(err, count) {
+                count.should.equal(3);
+                done();
+              });
+            });
+          });
+        });
+      });
+
+      it('should return exactly three messages', function(done) {
+        handler.getall(function(err, result) {
+          result.length.should.equal(3);
+          result = result.map(function(i) { return i['message']; });
+          result.should.eql(['message 1', 'message 2', 'message 3']);
+          done();
+        });
+      });
+    })
+
   })
 
   after(function(done) {
