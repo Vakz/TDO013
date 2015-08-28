e82c260bd55aaccacd18fe7f770ea1b4b9494777
Fredrik Jonsén <fredrikjonsen@gmail.com>
2015-08-28 15:39:00 +0200

    Remade into express server

--- lab2/lib/server.js
+++ lab2/lib/server.js
@@ -1,53 +1,51 @@
 // Add router to this file
 
-var http = require('http');
-var url = require('url');
 var RequestHandler = require('./requestHandler');
 
 var Server = function(collectionName) {
+
   collectionName = collectionName || 'chat';
   var requestHandler = new RequestHandler(collectionName);
   var outer = this;
   var connections = [];
-  var internalServer = http.createServer(function(request, response) {
-    outer.route(request, response);
-  });
-  internalServer.on('connection', function(socket) {
+
+  var app = (require('express'))();
+
+  app.on('connection', function(socket) {
     connections[connections.length] = socket;
     socket.on('close', function() {
       connections.splice(connections.indexOf(socket), 1);
     });
+  });
+
+  app.get('/flag', function(req, res) {
+    requestHandler.flag(req, res);
+  });
+
+  app.get('/save', function(req, res) {
+    requestHandler.save(req, res);
+  });
+
+  app.get('/getall', function(req, res) {
+    requestHandler.getall(req, res);
   })
 
+  app.get('*', function(req, res) {
+    res.sendStatus(404);
+  })
 
-  this.route = function(request, response) {
-    var urlParts = url.parse(request.url);
-    var path = urlParts.pathname.substring(1);
-
-    if (request.method == 'POST') {
-      response.writeHead(405, {'Content-Type': 'text/html'});
-      response.write("Wrong method, POST not allowed");
-      response.end();
-    }
-    else if (path !== '' && typeof requestHandler[path] === 'function') {
-      requestHandler[path](request, response);
-    }
-    else {
-      response.writeHead(404, {'Content-Type': 'text/html'});
-      response.write("404 Not Found");
-      response.end();
-    }
-  }
+  app.post('/', function(req, res) {
+    res.sendStatus(405)
+  })
 
   // Start accepting connections and connect to database
   this.start = function() {
-    internalServer.listen(8888);
+    app.listen(8888);
     requestHandler.connect();
   }
 
   // Stop all incoming connections and close db
   this.stop = function() {
-    internalServer.close();
     connections.forEach(function(socket) {
       socket.destroy();
     });
