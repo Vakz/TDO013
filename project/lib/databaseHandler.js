2285118d6c351ba8344e8b831c446cf24615c443
Fredrik Jonsén <fredrikjonsen@gmail.com>
2015-09-15 23:51:14 +0200

    Just testing branches, nothing changed

--- project/lib/databaseHandler.js
+++ project/lib/databaseHandler.js
@@ -5,34 +5,40 @@ var mongodb = require('mongodb');
 var config = require('./config');
 var ObjectID = mongodb.ObjectID;
 
-var DatabaseHandler = function() {
-  var db = null;
-  var connected = false;
-  var collections = {};
 
-  var getCollection = function(collection) {
-    if (!collections.hasOwnProperty(collection)) {
-      collections[collection] = db.collection(collection);
+var getCollection = function(collection) {
+  if (!collections.hasOwnProperty(collection)) {
+    collections[collection] = db.collection(collection);
+  }
+  return collections[collection];
+};
+var db;
+var connected;
+var collections_test;
+
+/*
+ * Deletes empty parameters. If a _id parameter is present, converts it
+ * to an ObjectID.
+ */
+var prepareParams = function(params) {
+  Object.keys(params).forEach(function(key) {
+    if (typeof params[key] !== 'string') return;
+    if (!params[key] || !params[key].trim())
+    {
+      delete params[key];
     }
-    return collections[collection];
-  };
+  });
+  if (params.hasOwnProperty('_id')) params._id = new ObjectID(params._id);
+};
 
-  /*
-   * Deletes empty parameters. If a _id parameter is present, converts it
-   * to an ObjectID.
-   */
-  var prepareParams = function(params) {
-    Object.keys(params).forEach(function(key) {
-      if (typeof params[key] !== 'string') return;
-      if (!params[key] || !params[key].trim())
-      {
-        delete params[key];
-      }
-    });
-    if (params.hasOwnProperty('_id')) params._id = new ObjectID(params._id);
-  };
+class DatabaseHandler {
+  constructor() {
+    db = null;
+    connected = false;
+    collections = {};
+  }
 
-  this.connect = function(callback) {
+  connect(callback) {
     if (connected) {
       /* istanbul ignore else */
       if (callback) callback(new errors.DatabaseError("Already connected"), null);
@@ -52,16 +58,16 @@ var DatabaseHandler = function() {
         if (callback) callback(null, true);
         connected = true;
       });
-  };
+  }
 
-  this.close = function(callback) {
+  close(callback) {
     if (db) db.close(callback);
     else callback(new errors.DatabaseError("Database already closed"), null);
     connected = false;
     db = null;
-  };
+  }
 
-  this.registerUser = function(params, callback) {
+  registerUser(params, callback) {
     // Make sure all are set
     if ([params.username, params.salt, params.password].some(s => !s || !s.trim())) {
       /* istanbul ignore else */
@@ -111,9 +117,9 @@ var DatabaseHandler = function() {
         }
       }
     });
-  };
+  }
 
-  this.getUser = function(params, callback) {
+  getUser(params, callback) {
     if(!connected) {
       /* istanbul ignore else */
       if (callback) callback(new errors.DatabaseError("Not connected to database"), null);
@@ -135,7 +141,7 @@ var DatabaseHandler = function() {
       if(res) res._id = res._id.toString();
       callback(null, res);
     });
-  };
-};
+  }
+}
 
 module.exports = DatabaseHandler;
